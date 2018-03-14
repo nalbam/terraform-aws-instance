8d215cc5382b43bc14096cc8fe29ee7584572668
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2018-03-14 14:44:21 +0900

    ami

--- amis.tf
+++
@@ -1,4 +0,0 @@
-module "ami" {
-  source = "git::https://gitlab.com/nalbam/terraform-aws-ami.git"
-  region = "${var.region}"
-}
--- main.tf
+++ main.tf
@@ -50,3 +50,8 @@ resource "aws_instance" "default" {
     ]
   }
 }
+
+module "ami" {
+  source = "git::https://gitlab.com/nalbam/terraform-aws-ami.git"
+  region = "${var.region}"
+}
